4a979644efc96cc6b27363065bad13b479e1f0a7
Matheus <matheus.sil.oli@hotmail.com>
2021-11-26 20:52:38 -0300

    Immediate fix

--- src/Immediate.class.php
+++ src/Immediate.class.php
@@ -3,6 +3,8 @@ namespace Fiqon;
 
 class Immediate extends Event {
     public function __construct(int $timeout = 240) {
+        parent::__construct();
+        
         $this->timeout = $timeout;
     }
 
